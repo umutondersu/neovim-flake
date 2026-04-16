d3925802e7745482a69e839808d2ac4e431d19b5
Umut Sahin Onder <umutsonder@gmail.com>
2026-04-16 17:56:36 +0200

    Support x86_64-linux and aarch64-linux via forAllSystems

--- flake.nix
+++ flake.nix
@@ -6,36 +6,47 @@
   outputs =
     { self, nixpkgs }:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      lib = pkgs.lib;
-
-      neovim-default = pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped {
-        plugins = [ ];
-        luaRcContent = "";
-        wrapRc = false;
-        withPython3 = false;
-        withRuby = false;
-        withNodeJs = false;
-        viAlias = false;
-        vimAlias = false;
-        wrapperArgs = [
-          "--suffix"
-          "PATH"
-          ":"
-          (lib.makeBinPath (
-            with pkgs;
-            [
-              xclip
-              xsel
-              wl-clipboard
-              tree-sitter
-            ]
-          ))
-        ];
-      };
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
     in
     {
-      packages.${system}.default = neovim-default;
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          lib = pkgs.lib;
+
+          neovim-default = pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped {
+            plugins = [ ];
+            luaRcContent = "";
+            wrapRc = false;
+            withPython3 = false;
+            withRuby = false;
+            withNodeJs = false;
+            viAlias = false;
+            vimAlias = false;
+            wrapperArgs = [
+              "--suffix"
+              "PATH"
+              ":"
+              (lib.makeBinPath (
+                with pkgs;
+                [
+                  xclip
+                  xsel
+                  wl-clipboard
+                  tree-sitter
+                ]
+              ))
+            ];
+          };
+        in
+        {
+          default = neovim-default;
+        }
+      );
     };
 }
